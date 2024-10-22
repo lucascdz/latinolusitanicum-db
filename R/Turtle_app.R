76c0545e8b063e8bba560ac382c4244b4df97b94
Lucas Dezotti <lucascdz@gmail.com>
2024-10-22 15:05:42 -0300

    Remember validate

--- R/Turtle_app.R
+++ R/Turtle_app.R
@@ -1,19 +1,19 @@
 # APP for building Turtle RDF files from Dictionary source datasets
 
-Dict <- 'Velez'
+#Dict <- 'Velez'
 #Dict <- 'Fonseca'
-#Dict <- 'Cardoso'
+Dict <- 'Cardoso'
 
 if(Dict=='Velez'){
    sourceDir <- "../latinolusitanicum-sources/Velez/Velez.1744.indextotiusartis.1.4.2/"
    sourcePrefix <- "Velez1744\\."
 }
 if(Dict=='Fonseca'){
-   sourceDir <- "./sources/Fonseca/Fonseca.1798.parvumlexicum-0.4.5/"
+   sourceDir <- "../latinolusitanicum-sources/Fonseca/Fonseca.1798.parvumlexicum-0.4.5/"
    sourcePrefix <- "Fonseca1798\\."
 }
 if(Dict=='Cardoso'){
-   sourceDir <- "./sources/Cardoso/Cardoso.1570.latinolusitanicum-0.2.8/"
+   sourceDir <- "../latinolusitanicum-sources/Cardoso/Cardoso.1570.latinolusitanicum-0.2.8/"
    sourcePrefix <- "Cardoso1570\\."
 }
 targetPub <- "LiLa"
@@ -24,6 +24,8 @@ source('./R/Turtle_pipeline.R')
 ## Velez total time = 30 sec.
 ## Fonseca total time = 07 min.
 ## Cardoso total time = 04 min.
+
+## REMEMBER VALIDATE TURTLE FILES THROUGH COMMAND LINE 'ttl [file]'
 
 ######
 # CONSIDER EXCLUDING ENTRIES WITHOUT LEXICAL SENSE (but preserve those mentioned in 'decomp')
